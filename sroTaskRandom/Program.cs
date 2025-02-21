1d812a50532204fee5f86b14a75cde57440da388
creatura <creatura@DESKTOP-18DVS2J>
2025-02-21 15:39:18 +0500

    Добавьте файлы проекта.

---
+++ sroTaskRandom/Program.cs
@@ -0,0 +1,18 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+
+string[] names = {
+    "Mike",
+    "Jake",
+    "Mark"
+};
+
+app.MapGet("/", () => "Sitemap:\n/access");
+
+app.MapGet("/access", () => {
+    var random = new Random();
+    var name = names[random.Next(names.Length)];
+    return new { message = name };
+});
+app.Run();
